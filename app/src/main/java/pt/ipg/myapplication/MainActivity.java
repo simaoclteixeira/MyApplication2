12efcb1f6399e4e23d33c605343b0e4bf2e62543
simaoclteixeira <sclteixeira11@gmail.com>
2019-03-25 14:53:35 +0000

    Initial commit

---
+++ app/src/main/java/pt/ipg/myapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package pt.ipg.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonCarros;
+    private Button buttonMotas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.buttonCarros);
+        Button button2 = findViewById(R.id.buttonMotas);
+        Button button3 = findViewById(R.id.buttonContatos);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+
+        }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+        case R.id.buttonCarros:
+            openActivity2();
+            break;
+        case R.id.buttonMotas:
+            openActivity3();
+            break;
+            case R.id.buttonContatos:
+                openActivity4();
+
+    }
+}
+
+
+
+        public void openActivity2() {
+            Intent intent = new Intent(this, Activity2.class);
+            startActivity(intent);
+        }
+
+        public void openActivity3() {
+            Intent intent = new Intent(this, Activity3.class);
+            startActivity(intent);
+        }
+
+    public void openActivity4() {
+        Intent intent = new Intent(this, Activity4.class);
+        startActivity(intent);
+    }
+
+}
