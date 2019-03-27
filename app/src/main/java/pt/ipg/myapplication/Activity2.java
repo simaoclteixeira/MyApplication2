65df1e883d37c23078571f54d83c67f8dff6c9d5
simaoclteixeira <sclteixeira11@gmail.com>
2019-03-27 19:54:27 +0000

    Acções dos botoes criadas. Botao pesquisar, mostra mensagem e volta ao menu, e botao cancelar volta ao menu

--- app/src/main/java/pt/ipg/myapplication/Activity2.java
+++ app/src/main/java/pt/ipg/myapplication/Activity2.java
@@ -3,6 +3,11 @@ package pt.ipg.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+
+        Button buttonCancelar = findViewById(R.id.buttonCancelar);
+        buttonCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Button buttonPesquisar = findViewById(R.id.buttonPesquisar);
+        buttonPesquisar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextFabricante = findViewById(R.id.editTextFabricante);
+                String mensagem = editTextFabricante.getText().toString();
+
+                if (mensagem.trim().length()==0) {
+                    editTextFabricante.setError(getString(R.string.obrigatorio_escrever));
+                }else if (!mensagem.matches("[a-zA-Z ]+")){
+                    editTextFabricante.setError(getString(R.string.caracteres_especiais));
+                }else{
+                    finish();
+                    Toast.makeText(Activity2.this,"A pesquisar...",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
